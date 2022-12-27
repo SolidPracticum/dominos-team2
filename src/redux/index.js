ba998d820d30985326edb7b618964e54134fc107
luchok22 <aitbaev2021@mail.ru>
2022-12-27 23:19:51 +0600

    feat:check redux

---
+++ src/redux/index.js
@@ -0,0 +1,14 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { SliceTestReducer } from "./Slice";
+
+
+
+const reducers = combineReducers({
+    slice: SliceTestReducer
+});
+
+export const store = configureStore({
+    reducer: reducers
+});
+
+console.log(store)
