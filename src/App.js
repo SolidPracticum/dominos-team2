559effdac4cd0de3e233b363c0c6eb626e3b36f2
luchok22 <aitbaev2021@mail.ru>
2023-01-05 20:33:20 +0600

    feat:clear app and width in %

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 
-
-import './App.css';
-
-import "./main.sass"
-
-
+import { Route, Routes } from "react-router-dom";
+import Cart from "./Components/Cart/Cart";
+import './Components/Cart/Cart.scss'
+import pizza from './pizza.json'
 
 function App() {
   return (
     <div className="App">
+      <div className="cart-f">
+        <Routes>
+          <Route path="/" element={pizza.map((item) => <Cart key={item.id} {...item} />)} />
+        </Routes>
+      </div>
     </div>
   );
 }
