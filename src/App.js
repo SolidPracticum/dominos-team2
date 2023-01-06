b278c26cc2fba13b692d6416db6c20f8b4f5aee5
luchok22 <aitbaev2021@mail.ru>
2023-01-06 16:28:38 +0600

    feat:promo main

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { Route, Routes } from "react-router-dom";
 import Cart from "./Components/Cart/Cart";
 import './Components/Cart/Cart.scss'
+import PromotionM from "./Components/PromotionM/PromotionM";
 import pizza from './pizza.json'
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <div className="App">
       <div className="cart-f">
         <Routes>
-          <Route path="/" element={pizza.map((item) => <Cart key={item.id} {...item} />)} />
+          {/* <Route path="/" element={pizza.map((item) => <Cart key={item.id} {...item} />)} /> */}
+          <Route path="/" element={<PromotionM />}/>
         </Routes>
       </div>
     </div>
