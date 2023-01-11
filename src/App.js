457fae6c297cecba3979dae2bcf3de9603b97720
luchok22 <aitbaev2021@mail.ru>
2023-01-11 12:22:21 +0600

    fix: bag in app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 
 import { Route, Routes } from "react-router-dom";
-import Cart from "./Components/Cart/Cart";
 import './Components/Cart/Cart.scss'
 import PromotionM from "./Components/PromotionM/PromotionM";
-import pizza from './pizza.json'
 
 function App() {
   return (
     <div className="App">
       <div className="cart-f">
         <Routes>
-          <Route path="/" element={<PromotionM />}/>
+          <Route path="/" element={<PromotionM/>}/>
         </Routes>
       </div>
     </div>
