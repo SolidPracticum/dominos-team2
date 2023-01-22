999bfb443806b0930ca6390ba8943585ec2a47e6
luchok22 <aitbaev2021@mail.ru>
2023-01-22 16:00:40 +0600

    fix: add header fixed

--- src/page/DessertsPage.jsx
+++ src/page/DessertsPage.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import DessertCart from '../component/DessertCart/DessertCart';
 import Header from '../component/Header/Header';
-import dessert  from './../component/Array/desserts.json'
+import dessert from './../component/Array/desserts.json'
 const DessertsPage = () => {
     console.log(dessert);
     const desert = [dessert]
     return (
         <div>
-            <Header/>
-            {desert.map((arr) =>
-                arr.map((item) =>
-                    <div className='MAIN'>
-                        <p className='text'>{item.text}</p>
-                        <div className='cart-f'>
-                            {item.desserts.map((i) => <DessertCart {...i} />)}
+            <Header />
+            <div className='content'>
+                {desert.map((arr) =>
+                    arr.map((item) =>
+                        <div className='MAIN'>
+                            <p className='text'>{item.text}</p>
+                            <div className='cart-f'>
+                                {item.desserts.map((i) => <DessertCart {...i} />)}
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+            </div>
         </div>
     );
 };
