391ec4f525cb6b3f33c05730c7a65a9da51e1197
luchok22 <aitbaev2021@mail.ru>
2023-02-01 15:42:07 +0600

    fix: delete Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
-import Footer from "./components/Footer/Footer"
+import Footer from "./component/Footer/Footer"
 import HomePage from "./page/HomePage";
 import './App.scss';
 import DrinksPage from './page/DrinksPage';
